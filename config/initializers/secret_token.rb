ab7bec07a6577e039339e02d98d1003258b43d03
arash <arash.danaie@gmail.com>
2014-10-03 19:53:14 -0400

    whoops!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,3 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 TapDatRails::Application.config.secret_token = 'b1f44987006b908a875f41f3fb77fe07f01c3f9647e7a8cd3bcfeb788aac0f8e574bbec0159452832b209f610f502ccf932d6a2bf23675c897a5042adb91d445'
-config.secret_token = '906535f428663858c80afe182b031390ece9ce38b67f7de9b001e23a8343a27bd24b105c7dc3c5a9985f8f1ded3e5e61a972acb924f5cc5bbd4a2f6f03c0dd3a'
