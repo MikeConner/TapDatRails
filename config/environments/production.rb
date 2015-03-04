c404c1687868d877e5f6f1f89084dc0d9a6fd55a
Arash <arash@tapdatapp.co>
2015-03-04 13:02:25 -0500

    debuggin in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,10 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+
+  # THIS WILL BE CHANGED BACK LATER:  TODO (For Debugging purposes only!)
+  config.consider_all_requests_local       = true
+
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
