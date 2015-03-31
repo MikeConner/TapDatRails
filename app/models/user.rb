ed3289ba2a71615c27ab9db27be933466b3c7a22
Arash <arash@tapdatapp.co>
2015-03-30 21:28:20 -0400

    Revert "reverting.  These changes broke uploading mobile thumb pics from the app"
    This reverts commit 59fd9289213f8e483476a78cd88e8ee6f97420fa.

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,12 +42,12 @@ require 'nickname_generator'
 #
 # NOTES AND WARNINGS
 #   Authenticates by token, through devise (not email/password). Email can be set by the user, but defaults to an auto-generated one
-# in a fake "UNKNOWN_EMAIL_DOMAIN".
+# in a fake "UNKNOWN_EMAIL_DOMAIN". 
 #
 class User < ActiveRecord::Base
   extend FriendlyId
   friendly_id :generate_slug, use: [:slugged, :history, :finders]
-
+  
   include ApplicationHelper
 
   before_save :ensure_authentication_token!
@@ -62,43 +62,41 @@ class User < ActiveRecord::Base
   # User Roles (bitmask, if we need more than one)
   # Wimping out and not using Devise mechanisms
   ADMIN_ROLE = 1
-
+  
   mount_uploader :profile_image, ImageUploader
-  mount_uploader :profile_thumb, ImageUploader
   mount_uploader :inbound_btc_qrcode, ImageUploader
   process_in_background :profile_image
   process_in_background :inbound_btc_qrcode
-  process_in_background :profile_thumb
-  
+                    
   has_many :nfc_tags, :dependent => :destroy
-  has_many :transactions, :dependent => :restrict_with_error
-  has_many :transaction_details, :through => :transactions
+  has_many :transactions, :dependent => :restrict_with_error 
+  has_many :transaction_details, :through => :transactions   
   has_many :currencies, :dependent => :destroy
   has_many :balances, :dependent => :destroy
-  has_many :vouchers, :dependent => :restrict_with_error
+  has_many :vouchers, :dependent => :restrict_with_error 
   has_many :payloads, -> { uniq }, :through => :transactions
-
+  
   validates :email, :uniqueness => { case_sensitive: false },
                     :format => { with: EMAIL_REGEX },
                     :allow_blank => true
   validates_presence_of :name
   validates :phone_secret_key, :presence => true, :length => { :maximum => SECRET_KEY_LEN }
-  validates :satoshi_balance, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
-
+  validates :satoshi_balance, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }  
+  
   def generated_email?
     self.email.ends_with?(UNKNOWN_EMAIL_DOMAIN)
-  end
-
+  end  
+  
   def admin?
     1 == (self.role & ADMIN_ROLE)
   end
-
+  
   def currency_balance(currency)
     balance = balances.where(:currency_id => currency.id).first rescue nil
-
+    
     balance.nil? ? 0 : balance.amount
   end
-
+  
   def reset_password
     pwd = NicknameGenerator.generate_nickname.gsub(/ /, '_') + '_' + SecureRandom.hex(2)
     self.password = pwd
@@ -106,7 +104,7 @@ class User < ActiveRecord::Base
       UserMailer.delay.welcome_email(self, pwd)
     end
   end
-
+  
 protected
   def ensure_authentication_token!
     if self.authentication_token.blank?
@@ -120,11 +118,11 @@ protected
       break token unless User.where(:authentication_token => token).first
     end
   end
-
+     
   def generate_secure_token_string
     SecureRandom.urlsafe_base64(25).tr('lIO0', 'sxyz')
   end
-
+  
 private
   def generate_slug
     SecureRandom.uuid
