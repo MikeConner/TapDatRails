d01725397f12ba9e3eecb525d6fa278e0c84adb6
Arash <arash@tapdatapp.co>
2015-08-21 04:47:37 -0400

    last bits for dynamic leaderboard

--- app/assets/javascripts/leader2.js
+++ app/assets/javascripts/leader2.js
@@ -1,38 +1,101 @@
-var myVar=setInterval(function(){myTimer()},5000);
-function myTimer() {
-    var d = new Date();
-    varJsonURL = window.location.href + ".json";
+$(document).ready(function() {
+  // Handler for .ready() called.
+  firsttime();
 
+  var myVar=setInterval(function(){myTimer()},1000);
+
+
+});
+
+var lastCheck;
+
+function firsttime() {
+  varJsonURL = window.location.href + ".json";
+  $.getJSON(  varJsonURL, function( data ) {
+    lastCheck = data;
+    var leaders = data[0];
+    var tapped = leaders["tapped"];
+    var latest = leaders["latest"];
+    var tappers = leaders["tappers"];
+
+    //if there is a chang or first time
+    updatelatest(latest);
+    updatetappers(tappers);
+    updatetags(tapped);
+
+    });
+};
+
+function myTimer() {
+    varJsonURL = window.location.href + ".json";
     $.getJSON(  varJsonURL, function( data ) {
 
-//JSON.stringify(leaders)
+      if( JSON.stringify(lastCheck) == JSON.stringify(data)) {
+          console.log("no change");
+      }
+      else{
+        lastCheck = data;
+        var leaders = data[0];
+        var tapped = leaders["tapped"];
+        var latest = leaders["latest"];
+        var tappers = leaders["tappers"];
 
-
-      var leaders = data[0];
-      var tapped = leaders["tapped"];
-      var latest = leaders["latest"];
-      var tappers = leaders["tappers"];
-
-$("#latestname").html(latest[0]["name"]);
-$("#latestamount").html(latest[0]["amount"]);
-$("#latesttag").html(latest[0]["tag"]);
-$("#latesttagimage").html(latest[0]["yapa"]);
-$("#latestuserimage").html(latest[0]["image"]);
-
-document.getElementById("demo").innerHTML = JSON.stringify(tappers);
-
-
-
-//go through each array, compare and update values in list below
-
-
-
-//      $.each( data, function( key, val ) { items.push( "<li id='" + key + "'>" + val + "</li>" );});
-
-//      $( "<ul/>", { "class": "my-new-list", html: items.join( "" ) }).appendTo( "body" );
+        //if there is a chang or first time
+        updatelatest(latest);
+        updatetappers(tappers);
+        updatetags(tapped);
+      }
     });
+};
 
-    //d.toLocaleTimeString();
+function updatetags(tapped){
+  var len = tapped.length;
+  for (i=1;  i<= len; i++){
+
+    $("#tagrank"+i).html(tapped[i-1]["rank"]);
+    $("#tagname"+i).html(tapped[i-1]["name"]);
+    $("#tagamount"+i).html("$"+tapped[i-1]["total"]);
+    $("#tagtaps"+i).html(tapped[i-1]["taps"]);
+
+    if (tapped[i-1]["image"]==""){
+      $("#tagimage"+i).html("<img src='/assets/deflb1.png'>");
+    }else{
+      $("#tagimage"+i).html("<img src='" + tapped[i-1]["image"]+ "'>");
+    }
+
+  }
 
 
 }
+
+
+function updatetappers(tappers){
+  var len = tappers.length;
+  for (i=1;  i<= len; i++){
+
+    $("#rank"+i).html(tappers[i-1]["rank"]);
+    $("#tapname"+i).html(tappers[i-1]["name"]);
+    $("#tapamount"+i).html("$"+tappers[i-1]["total"]);
+    $("#taptaps"+i).html(tappers[i-1]["taps"]);
+
+    if (tappers[i-1]["image"]==""){
+      $("#tapimage"+i).html("<img src='/assets/deflb1.png'>");
+    }else{
+      $("#tapimage"+i).html("<img src='" + tappers[i-1]["image"]+ "'>");
+    }
+
+  }
+
+
+}
+
+
+function updatelatest(latest){
+
+  $("#latestname").html(latest[0]["name"]);
+  $("#latestamount").html(latest[0]["amount"]);
+  $("#latesttag").html(latest[0]["tag"]);
+  $("#latesttagimage").html("<img src='" + latest[0]["yapa"] + "'>");
+  $("#latestuserimage").html("<img src='" + latest[0]["image"]+ "'>");
+
+};
